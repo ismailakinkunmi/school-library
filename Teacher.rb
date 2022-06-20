c01e17f12743cdf5aa5dcd4cf6e711a5902506a8
Adekunle Ismail <adismail4@gmail.com>
2022-06-20 22:30:33 +0100

    Add teacher.rb

---
+++ Teacher.rb
@@ -0,0 +1,11 @@
+class Teacher < Person
+  attr_accessor = specialization
+  def initialize(specialization, age, name)
+    @specialization = specialization
+    super(age, name)
+  end
+  
+  def can_use_services?
+   true
+  end
+end
