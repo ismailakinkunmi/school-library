e6fe342bc23aeaa87c86aa034967b3f52bf7e411
Adekunle Ismail <adismail4@gmail.com>
2022-06-20 22:28:19 +0100

    Add student.rb

---
+++ Student.rb
@@ -0,0 +1,11 @@
+class Student < Person
+  attr_accessor = classroom
+  def initialize (classroom, age, name)
+    super(age, name)
+    @classroom = classroom
+  end
+  
+  def play_hooky
+    "¯\(ツ)/¯".
+  end
+end
