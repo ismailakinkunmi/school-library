576f18cca55a3613c2604a9f254f38c3b4cab395
Adekunle Ismail <adismail4@gmail.com>
2022-06-20 23:08:37 +0100

    Update person.rb to fix rubocop error

--- Person.rb
+++ Person.rb
@@ -8,14 +8,14 @@ class Person
     @age = age
     @parent_permission = parent_permission
   end
-  
+
   def of_age?
     @age >= 18
   end
-  
+
   def can_use_services
     is_of_age || parent_permission     
   end
-  
-  private :of_age?
+
+  private :of_age?  
 end
