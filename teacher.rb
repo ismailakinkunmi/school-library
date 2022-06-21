80eb548433fac563713036a110ccb91d91266cd4
Adekunle Ismail <adismail4@gmail.com>
2022-06-21 06:52:05 +0100

    Update teacher.rb

--- teacher.rb
+++ teacher.rb
@@ -1,9 +1,11 @@
+require './person'
+
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(specialization, age, name)
+  def initialize(specialization, age, name = 'unknown', parent_permission: true)
     @specialization = specialization
-    super(age, name)
+    super(age, name, parent_permission)
   end
 
   def can_use_services?
