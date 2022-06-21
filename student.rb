e615694ead4fc915a9486e8824d08e92ddc6b93a
Adekunle Ismail <adismail4@gmail.com>
2022-06-21 06:50:58 +0100

    Update student.rb

--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require './person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(classroom, age, name)
-    super(age, name)
+  def initialize(classroom, age, name = 'unknown', parent_permission: true)
+    super(age, name, parent_permission)
     @classroom = classroom
   end
 
