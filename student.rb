ce5de7681216c1ae209a5df4e9824df920022e3f
Adekunle Ismail <adismail4@gmail.com>
2022-06-23 00:42:33 +0100

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,13 +1,19 @@
 require './person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
-  def initialize(age, name = 'unknown', _classroom)
+  def initialize(age, name = 'unknown')
+    @classroom = classroom
     super(age, name)
   end
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.student.push(self) unless classroom.student.include?(self)
   end
 end
