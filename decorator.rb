2ea7c2505a5f16570465c84b315c14149384ae5d
Adekunle Ismail <adismail4@gmail.com>
2022-06-22 09:38:22 +0100

    update decorator.rb for rubocop error

--- decorator.rb
+++ decorator.rb
@@ -1,12 +1,10 @@
 class Nameable
-
   def correct_name
     raise 'NotImplementedError'
   end
 end
 
 class Decorator < Nameable
-
   attr_accessor :nameable
 
   def initialize(nameable)
@@ -16,19 +14,15 @@ class Decorator < Nameable
   def correct_name
     @nameable.correct_name
   end
-
 end
 
 class CapitalizeDecorator < Decorator
-
   def correct_name
     @nameable.correct_name.upcase
   end
-
 end
 
 class TrimmerDecorator < Decorator
-  
   def correct_name
     @nameable.correct_name.slice(0, 9)
   end
