3678ed32c4373673a58ecd0d8734bd2881a24893
Adekunle Ismail <adismail4@gmail.com>
2022-06-22 09:38:52 +0100

    update person.rb for rubocop error

--- person.rb
+++ person.rb
@@ -6,13 +6,13 @@ class Person < Nameable
 
   def initialize(age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1..1000)
-    @name = name    
+    @name = name
     @age = age
-    @parent_permission = parent_permission    
+    @parent_permission = parent_permission
   end
 
   def of_age
-    @age >= 18    
+    @age >= 18
   end
 
   def can_use_services
