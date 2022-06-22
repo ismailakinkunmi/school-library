7260d6af263096cfea324d20b1bdfa53c8bf555f
Adekunle Ismail <adismail4@gmail.com>
2022-06-22 09:14:23 +0100

    Add person.rb

--- person.rb
+++ person.rb
@@ -1,21 +1,27 @@
-class Person
+require './decorator'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
   def initialize(age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1..1000)
-    @name = name
+    @name = name    
     @age = age
-    @parent_permission = parent_permission
+    @parent_permission = parent_permission    
   end
 
-  def of_age?
-    @age >= 18
+  def of_age
+    @age >= 18    
   end
 
   def can_use_services
-    is_of_age || parent_permission
+    of_age || parent_permission
   end
 
-  private :of_age?
+  def correct_name
+    @name
+  end
+
+  private :of_age
 end
