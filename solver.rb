29fbf3a3f4be1ceb6c1c7cf2add202eedc642ad7
Adekunle Ismail <adismail4@gmail.com>
2022-06-30 22:16:39 +0100

    Add solver and other test

--- solver.rb
+++ solver.rb
@@ -1,11 +1,24 @@
 class Solver
+  def factorial(number)
+    return 'Can not have factorial of a negative number' if number.negative?
+    return 1 if number.zero?
 
-    def factorial(number)
-        return 'Can not have factorial of a negative number' if number.negative?
-        return 1 if number == 0
-        number * factorial(number - 1)
+    number * factorial(number - 1)
+  end
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizz_buzz(number)
+    if (number % 3).zero? and (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number.to_s
     end
+  end
 end
-
-checker = Solver.new
-puts checker.factorial(-1)
